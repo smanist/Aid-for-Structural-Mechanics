249a32515858c88994988a9f651a77c021f49fb8
smanist <huangdn.nuaa@gmail.com>
2014-03-05 13:15:48 -0500

    Updated mainly the varD1D and hamiltonP functions

--- StructuralMechanics.m
+++ StructuralMechanics.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
 BeginPackage["StructuralMechanics`"]
-(*V0 .4*)
+(*V0 .5*)
 
 (*Foundamental functions*)
 energyParser::usage = "energyParser[EXPR, FUN, VAR, TAR] returns EXPR whose FUN[VAR,t] are replaced with TAR."
@@ -26,71 +26,72 @@ energyParser[EXPR_,FUN_,VAR_,TAR_]:=Module[{expr=Expand[EXPR],fun=FUN,var=VAR,ta
 cvt=Flatten@Table[ToExpression@StringJoin[ToString[fun[[k]]],Table[ToString[var],{i}],Table["t",{j}]]->D[tar[[k]],{var,i},{ToExpression["t"],j}],{i,0,const},{j,0,const},{k,1,Length[fun]}];
 expr/.cvt];
 
-varD1D[EXPR_,FUN_,VAR_]:=Module[{expr=EXPR,fun=FUN,var=VAR,t=ToExpression["t"],res,df,tmp,tmpVar,prt},
+varD1D[EXPR_,FUN_,VAR_]:=Module[{expr=EXPR,fun=FUN,var=VAR,t=ToExpression["t"],res,df,tmp,tmpVar,prt,prt0},
 (*Variantional operation on Integrals based on Gateaux derivative*)
+(*Specialized for Hamilton's Principle*)
 res=Table[
 (*Construct variational symbol*)
 df=ToExpression["\[Delta]"<>ToString[f]];
 (*Gateaux derivative*)
 tmp=D[expr/.{f[a__]->(f[a]+g\[Epsilon] df[a]),Derivative[a__][f][b__]->Derivative[a][f][b]+g\[Epsilon] Derivative[a][df][b]},g\[Epsilon]]/.{g\[Epsilon]->0};
-If[var===t,
-(*If integral variable is time*)
+(*Modify terms with temporal variable*)
 prt={
 Coefficient[tmp/.{Derivative[0,1][df][_,t]->tmpVar},tmpVar],
 Coefficient[tmp/.{Derivative[1,1][df][_,t]->tmpVar},tmpVar],
 Coefficient[tmp/.{Derivative[2,1][df][_,t]->tmpVar},tmpVar]};
-{df,-D[prt[[1]],t],-D[prt[[2]],t],-D[prt[[3]],t]},
-(*If integral variable is space*)
+prt0=tmp-prt[[1]]D[df[var,t],t]-prt[[2]]D[df[var,t],t,x]-prt[[3]]D[df[var,t],t,x,x];
+(*terms not related to time + terms from dt + terms from dtx + terms from dtxx*)
+tmp=prt0-D[prt[[1]],t]df[var,t]-D[prt[[2]],t]D[df[var,t],x]-D[prt[[3]],t]D[df[var,t],x,x];
+(*Modify terms with spatial variable*)
 prt={
 Coefficient[tmp/.{df[var,t]->tmpVar},tmpVar],
 Coefficient[tmp/.{Derivative[1,_][df][var,_]->tmpVar},tmpVar],
 Coefficient[tmp/.{Derivative[2,_][df][var,_]->tmpVar},tmpVar]};
-{df,prt[[1]]-D[prt[[2]],var]+D[prt[[3]],var,var],prt[[2]]-D[prt[[3]],var],prt[[3]]}]
+(*{var, eqn, BC:var, BC:Dvar}*)
+{df,prt[[1]]-D[prt[[2]],var]+D[prt[[3]],var,var],prt[[2]]-D[prt[[3]],var],prt[[3]]}
 ,{f,fun}];
 (*Result*)
 res];
 
 hamiltonP[EXPR_,FUN_,VAR_]:=
-Module[{expr=EXPR,fun=FUN,var=VAR,tuw,\[CapitalLambda]=ToExpression[{"\[Lambda]l","\[Lambda]r"}],t=ToExpression["t"],eb,eqT,eqU,eqTb,eqUb,bcl,bcr,res},
+Module[{expr=EXPR,fun=FUN,var=VAR,tuw,\[CapitalLambda]=ToExpression[{"\[Lambda]l","\[Lambda]r"}],t=ToExpression["t"],eb,eqd,eqb,bcl,bcr,res},
 (*Derive Hamiltonian EOM of a beam with BC's on both ends*)
-(*Convert energy expression to normal form*)
-tuw=energyParser[expr,fun,var,Table[f[var,t],{f,fun}]];
-(*Seperate domain terms and boundary terms*)
-eb=CoefficientArrays[tuw,\[CapitalLambda]]//Normal;
-eb=If[Length[eb]==1,{eb[[1]],Table[{0,0},{Dimensions[eb][[2]]}]},eb];
-(*Do variation on kinetic energy and the rest, domain terms*)
-eqT=varD1D[eb[[1,1]],fun,t];
-eqU=varD1D[eb[[1,2]]-eb[[1,3]],fun,var]; (*External work is included*)
-(*Do variation on kinetic energy and the rest, boundary terms*)
-eqTb=varD1D[eb[[2,1]],fun,t];
-eqUb=varD1D[eb[[2,2]]-eb[[2,3]],fun,var]; (*External work is included*)
+(*Convert energy expression to normal form, and Seperate domain terms and boundary terms*)
+tuw=Normal[CoefficientArrays[#,\[CapitalLambda]]]&[energyParser[-expr[[1]]+expr[[2]]-expr[[3]],fun,var,Table[f[var,t],{f,fun}]]];
+tuw=If[Length[tuw]==1,{tuw[[1]],{0,0}},tuw];
+(*Do variation on energy, domain terms*)
+eqd=varD1D[tuw[[1]],fun,var];
+(*Do variation on energy, boundary terms*)
+eqb=varD1D[tuw[[2]],fun,var];
 (*Assemble the EOM*)
 res=Table[
-bcl={
-{fun[[idx]][var,t],-eqU[[idx,3]]-eqT[[idx,3]]+eqTb[[idx,2,1]]+eqUb[[idx,2,1]]},
-{D[fun[[idx]][var,t],var],-eqU[[idx,4]]-eqT[[idx,4]]+eqTb[[idx,3,1]]+eqUb[[idx,3,1]]}};
-bcr={
-{fun[[idx]][var,t],eqU[[idx,3]]-eqT[[idx,3]]+eqTb[[idx,2,2]]+eqUb[[idx,2,2]]},
-{D[fun[[idx]][var,t],var],eqU[[idx,4]]-eqT[[idx,4]]+eqTb[[idx,3,2]]+eqUb[[idx,3,2]]}};
-{eqT[[idx,1]],eqU[[idx,2]]-eqT[[idx,2]],bcl,bcr}
-,{idx,1,Length[fun]}];
+(*The variable*)
+{eqd[[idx,1]],
+(*The equation*)
+eqd[[idx,2]],
+(*The left BC*)
+{{fun[[idx]][var,t],-eqd[[idx,3]]+eqb[[idx,2,1]]},
+{D[fun[[idx]][var,t],var],-eqd[[idx,4]]+eqb[[idx,3,1]]}},
+(*The right BC*)
+{{fun[[idx]][var,t],eqd[[idx,3]]+eqb[[idx,2,2]]},
+{D[fun[[idx]][var,t],var],eqd[[idx,4]]+eqb[[idx,3,2]]}}},
+{idx,1,Length[fun]}];
 res
 ];
 
 lagrangeP[EXPR_,FUN_,SFUN_,VAR_,RNG_]:=
 Module[{expr=EXPR,fun=FUN,sfun=SFUN,var=VAR,rng=RNG,\[CapitalLambda]=ToExpression[{"\[Lambda]l","\[Lambda]r"}],t=ToExpression["t"],len=Length@Flatten@SFUN,
-qfun,cnt,tar,lentmp,tuw,tuws,ener,dq,KK,MM,res,f0,fq,fdq},
+qfun,cnt,tar,lentmp,tuw,ener,dq,KK,MM,res,f0,fq,fdq},
 (*Derive Lagrangian EOM of a beam with BC's on both ends*)
 (*Convert energy expression to discretized form*)
 qfun=Table[ToExpression["q"<>ToString[idx]<>"[t]"],{idx,1,len}];
 cnt=0;
 tar=Table[lentmp=Length[sfun[[idx]]];cnt=cnt+lentmp;qfun[[cnt-lentmp+1;;cnt]].sfun[[idx]],{idx,1,Length@sfun}];
-tuw=energyParser[expr,fun,var,tar];
 (*Seperate domain terms and boundary terms*)
-tuws=CoefficientArrays[tuw[[1]]-tuw[[2]]+tuw[[3]],\[CapitalLambda]]//Normal;
-tuws=If[Length[tuws]==1,{tuws[[1]],{0,0}},tuws];
+tuw=CoefficientArrays[energyParser[expr[[1]]-expr[[2]]+expr[[3]],fun,var,tar],\[CapitalLambda]]//Normal;
+tuw=If[Length[tuw]==1,{tuw[[1]],{0,0}},tuw];
 (*Derive the total energy, including boundary terms*)
-ener=Expand[Integrate[tuws[[1]],Flatten@{var,rng}]+(tuws[[2,1]]/.{var->rng[[1]]})+(tuws[[2,2]]/.{var->rng[[2]]})];
+ener=Expand[Integrate[tuw[[1]],Flatten@{var,rng}]+(tuw[[2,1]]/.{var->rng[[1]]})+(tuw[[2,2]]/.{var->rng[[2]]})];
 (*Derive the K and M matrix*)
 dq=D[qfun,t];
 KK=Table[D[ener,{qfun[[i]],1},{qfun[[j]],1}],{i,1,len},{j,1,len}];
@@ -170,6 +171,9 @@ coeAppd[EXPR_,LISTN_,APPD_]:=EXPR/.Table[lst->ToExpression[ToString[lst]<>APPD],
 
 End[]
 EndPackage[]
+
+
+
 
 
 
